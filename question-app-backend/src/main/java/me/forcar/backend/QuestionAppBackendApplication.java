6487e1e70305f598829f7732fc25e25c87d28b87
= <=>
2021-04-21 14:28:59 -0300

    Adicionado o projeto spring que servira como backend do app de questao (versao ingles)

---
+++ question-app-backend/src/main/java/me/forcar/backend/QuestionAppBackendApplication.java
@@ -0,0 +1,13 @@
+package me.forcar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAppBackendApplication.class, args);
+	}
+
+}
---
+++ question-app-backend/src/test/java/me/forcar/backend/QuestionAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.forcar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
