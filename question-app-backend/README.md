edd072d713c1c757b714c7c96d5b3cd92de95176
= <=>
2021-04-21 16:39:24 -0300

    Adicionado documentacao da api com o Swagger/SpringFox

---
+++ question-app-backend/README.md
@@ -0,0 +1,11 @@
+# Api do aplicativo de questão
+
+### Como Acessar a url da documentacao da API?
+
+Para acessar a documetação da api a url segue o seguinte padrão:
+
+* http://host:port/application-name/swagger-ui/
+
+Caso esteja rodando o projeto na IDE basta acessar no seu browser a url:
+
+* http://localhost:8080/swagger-ui/
---
+++ question-app-backend/src/main/java/me/forcar/backend/configuration/SpringFoxConfiguration.java
@@ -0,0 +1,37 @@
+package me.forcar.backend.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringFoxConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("me.forcar.backend.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Question app API",
+                "API for a question app",
+                "0.0.1",
+                null,
+                new Contact("Gabriel Gonoring Borges", "https://github.com/gabrielgonoring", ""),
+                "", "", Collections.emptyList());
+    }
+}
