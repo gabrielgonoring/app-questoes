f106d7b957e25d3d6b8fcd0d325a3eda2e0599f7
= <=>
2021-04-21 13:48:49 -0300

    Adicionado o projeto Spring para o backend do app

---
+++ backend-app-questao/src/test/java/me/forcar/backendappquestao/BackendAppQuestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.forcar.backendappquestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppQuestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
