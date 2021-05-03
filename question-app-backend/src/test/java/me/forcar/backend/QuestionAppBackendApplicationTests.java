281855c332ce06c6daa59afeb67543e623d95f60
= <=>
2021-05-02 21:14:14 -0300

    Retirado o teste geito por padrao pelo spring do QuestionAppBackendApplicationTests

--- question-app-backend/src/test/java/me/forcar/backend/QuestionAppBackendApplicationTests.java
+++ question-app-backend/src/test/java/me/forcar/backend/QuestionAppBackendApplicationTests.java
@@ -3,11 +3,11 @@ package me.forcar.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class QuestionAppBackendApplicationTests {
 
-	@Test
+
+	//@Test
 	void contextLoads() {
 	}
-
 }
